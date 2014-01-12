9b707cf96f8ae4db7785ef8fbe8f4017b76f26c3
Prescott Murphy <prurph@gmail.com>
2014-01-12 17:56:57 -0500

    Finished about_dice_project.rb (216 koans completed)

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -2,9 +2,16 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_accessor :values
+
+  def roll(rolls)
+    @values = [] # Initializing here allows rerolling
+    rolls.times do
+      @values << rand(1..6)
+    end
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
