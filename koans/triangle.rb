81e75f345ea6ba02b558dec3c58457ca0c07af78
Prescott Murphy <prurph@gmail.com>
2014-01-12 13:26:48 -0500

    Finished about_triangle_project_2 (161 koans completed)

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,8 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  sides_array = [a, b, c]
-  [:equilateral, :isosceles, :scalene][sides_array.uniq.length - 1]
+  sides = [a, b, c].sort
+  raise TriangleError if sides[0] <= 0 || sides[0] + sides[1] <= sides[2]
+  [:equilateral, :isosceles, :scalene][sides.uniq.length - 1]
 end
 
 # Error class used in part 2.  No need to change this code.
